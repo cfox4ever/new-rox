f708648b6d5bcc64467d808bcf21988a0860eaf4
ethan roman <31373652+cfox4ever@users.noreply.github.com>
2024-03-25 13:12:36 -0400

    Index print port

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   const password = process.env.PASSWORD;
+  console.log(`Password: ${password}`);
+
+  if (!password) {
+    console.log('PASSWORD environment variable not set');
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('PASSWORD environment variable not set');
+    return;
+  }
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
